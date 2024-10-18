cfa7593a3cde48cc604646254a6e9d08921672ca
Meng Sen <qyg2297248353@gmail.com>
2024-10-18 15:34:16 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TARGET_DIR="/var/www/html"
+TEMP_DIR="/tmp/html-temp"
+
+if [ ! -d "$TARGET_DIR/config" ] || [ -z "$(ls -A $TARGET_DIR/config)" ]; then
+    cp -r $TEMP_DIR/config $TARGET_DIR/
+else
+    echo "Config directory exists, skipping copy."
+fi
+
+rsync -a --exclude 'config' --exclude 'docker-entrypoint.sh' $TEMP_DIR/ $TARGET_DIR/
+
+chown -R www-data:www-data $TARGET_DIR
+
+exec apache2-foreground
